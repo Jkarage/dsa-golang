e6aa41773ca9c98fe5a0e597700b13d843b357eb
Jkarage <josephbkarage@gmail.com>
2022-12-13 12:16:54 +0300

    Implementing queue with two stacks

---
+++ Data Structures/Queues/queueWithTwoStacks.go
@@ -0,0 +1,62 @@
+package queueII
+
+type Stack []int
+
+func (s *Stack) IsEmpty() bool {
+	return len(*s) == 0
+}
+
+func (s *Stack) Pop() int {
+	i := len(*s) - 1
+	popped := (*s)[i]
+	*s = (*s)[:i]
+	return popped
+}
+
+func (s *Stack) Push(i int) {
+	*s = append(*s, i)
+}
+
+func (s *Stack) Peek() int {
+	i := len(*s) - 1
+	return (*s)[i]
+}
+
+type Queue struct {
+	stack1 Stack
+	stack2 Stack
+}
+
+func (q *Queue) Enqueue(i int) {
+	q.stack1.Push(i)
+}
+
+func (q *Queue) Dequeue() {
+	if q.IsEmpty() {
+		panic("An empty Queue")
+	}
+	q.MoveToStack2()
+	q.stack2.Pop()
+}
+
+func (q Queue) IsEmpty() bool {
+	return q.stack1.IsEmpty() && q.stack2.IsEmpty()
+}
+
+func (q Queue) Peek() int {
+	if q.IsEmpty() {
+		panic("an empty stack")
+	}
+	q.MoveToStack2()
+
+	return q.stack2.Peek()
+}
+
+func (q Queue) MoveToStack2() {
+	if q.stack2.IsEmpty() {
+		for !q.stack1.IsEmpty() {
+			popped := q.stack1.Pop()
+			q.stack2.Push(popped)
+		}
+	}
+}
