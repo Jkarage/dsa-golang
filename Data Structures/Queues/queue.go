a1ee6e20ce89743d5129427d3df0a261a7388693
Jkarage <josephbkarage@gmail.com>
2022-12-13 07:28:12 +0300

    Implenented Queue using Slices

---
+++ Data Structures/Queues/queue.go
@@ -0,0 +1,46 @@
+package queue
+
+import "fmt"
+
+type ArrayQueue struct {
+	Q     []int
+	front int
+	rear  int
+	count int
+}
+
+func (q *ArrayQueue) Enqueue(a int) {
+	if q.IsEmpty() {
+		q.Q = append(q.Q, a)
+		q.count++
+		return
+	} else {
+		q.Q = append(q.Q, a)
+		q.rear++
+		q.count++
+	}
+}
+
+func (q *ArrayQueue) Dequeue() {
+	if q.IsEmpty() {
+		panic("An empty queue")
+	}
+	if q.front < q.rear {
+		q.Q = q.Q[q.front+1:]
+		q.rear--
+		q.count--
+	} else if q.front == q.rear {
+		q.Q = nil
+		q.count--
+	}
+}
+
+func (q *ArrayQueue) IsEmpty() bool {
+	return len(q.Q) == 0
+}
+
+func main() {
+	var q ArrayQueue
+
+	fmt.Println(q)
+}
