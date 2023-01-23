57598c9a310a703f866b21df98f083ada2884a89
Jkarage <josephbkarage@gmail.com>
2023-01-23 19:18:52 +0300

    Binary search implementation

---
+++ Algorithms/BinarySearch/binary_search.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	n := flag.Int("needle", 0, "a value to be searched in the slice")
+	flag.Parse()
+	h := []int{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21}
+	fmt.Println(BinarySearch(h, *n))
+}
+
+func BinarySearch(h []int, n int) bool {
+	lo := 0
+	hi := len(h)
+	for lo < hi {
+		m := lo + ((hi - lo) / 2)
+		if h[m] == n {
+			return true
+		} else if h[m] < n {
+			lo = m + 1
+		} else {
+			hi = m
+		}
+	}
+	return false
+
+}
--- go.mod
+++ go.mod
@@ -4,8 +4,6 @@ go 1.19
 
 require (
 	github.com/sirupsen/logrus v1.9.0 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/exp v0.0.0-20221217163422-3c43f8badb15 // indirect
+	golang.org/x/sys v0.1.0 // indirect
 )
-
-// 0699990300
-// 122882
